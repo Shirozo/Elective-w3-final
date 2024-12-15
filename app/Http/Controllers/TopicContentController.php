8613308f670c6079927a8b5522b5ad6140cf1d62
Neil Bryan Caranzo <caranzoneilbryan@gmail.com>
2024-12-16 01:02:39 +0800

    Done implementation

---
+++ app/Http/Controllers/TopicContentController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Content;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TopicContentController extends Controller
+{
+    public function show(Request $request) {
+        $contents = Content::where("topic_id", "=", $request->q)->get();
+
+        return view("topiccontent", [
+            "data" => $contents,
+            "id" => $request->q
+        ]);
+    }
+
+    public function store(Request $request) {
+        $data = Validator::make($request->all(), [
+            "title" => "required|min:1",
+            "content" => "required",
+            "id" => "required"
+        ]);
+
+        if ($data->fails()) {
+            return response()->json([
+                "message" => "Validation Fails!"
+            ], 403);
+        }
+
+        Content::create([
+            "title" => $request->title,
+            "youtube_link1" => $request->v1,
+            "youtube_link2" => $request->v2,
+            "content" => $request->content,
+            "topic_id" => $request->id
+        ]);
+
+        return response()->json([
+            "message" => "Content Added!"
+        ], 200);
+    }
+
+    public function delete(Request $request)
+    {
+        if ($request->has("delete_id")) {
+
+            $data = Content::find($request->delete_id);
+
+            if ($data != null) {
+
+                $data->delete();
+
+                return response()->json([
+                    "message" => "Data Deleted!"
+                ], 200);
+            }
+
+            return response()->json([
+                "message" => "Data not found!"
+            ], 403);
+        }
+
+        return response()->json([
+            "message" => "ID is required"
+        ], 404);
+    }
+
+    public function update(Request $request) {
+        $data = Validator::make($request->all(), [
+            "title" => "required|min:1",
+            "content" => "required",
+            "id" => "required"
+        ]);
+
+        if ($data->fails()) {
+            return response()->json([
+                "message" => "Validation Fails!",
+                "f" => $data->failed()
+            ], 403);
+        }
+
+        $instance = Content::find($request->id);
+
+        $instance->update([
+            "title" => $request->title,
+            "youtube_link1" => $request->youtube_link1,
+            "youtube_link2" => $request->youtube_link2,
+            "content" => $request->content,
+        ]);
+
+        return response()->json([
+            "message" => "Content Updated!"
+        ], 200);
+    }
+
+    public function api(Request $request) {
+        if ($request->has("id")) {
+            $data = Content::find($request->id);
+    
+            if ($data) {
+                return response()->json([
+                    "message" => "Data found!",
+                    "data" => $data
+                ], 200);
+            }
+    
+            return response()->json([
+                "message" => "Data not found!"
+            ], 404);
+        }
+    
+        return response()->json([
+            "message" => "ID is required"
+        ], 400);
+    }
+    
+}
